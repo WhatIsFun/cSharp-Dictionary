6db0678e9e90355e015f7962221c50a4a7e7f872
WhatIsFun <mohammed110051@gmail.com>
2023-07-13 12:26:14 +0400

    cSharp-Dictionary

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Dictionary
+{
+    internal class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+        public Person(long Id, string Name, string Gender) 
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Gender = Gender;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nGender: {Gender}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace cSharp_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1101, "Omasa", "Male");
+            Person person2 = new Person(1239, "Salma", "Female");
+            Person person3 = new Person(1834, "Ahmed", "Male");
+            Dictionary<long, Person> personMap = new Dictionary<long, Person>();
+            //personMap.Add(person1.Id, person1);  //\Add method 1
+            //personMap.Add(person2.Id, person2);
+            //personMap.Add(person3.Id, person3);
+            personMap[person1.Id] = person1; // Add method 2
+            personMap [person2.Id] = person2;
+            personMap [person3.Id] = person3;
+            person1.Gender = "Female"; // Change the gender
+            personMap[person1.Id] = person2; // Change something
+            personMap.TryAdd(person1.Id, person1); // 
+            Person? result = null;
+
+            if (personMap.TryGetValue(person2.Id, out result))
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
