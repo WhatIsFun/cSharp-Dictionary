946cbdcd035e322847f4ecd86d23a49ab71dbb84
Mohammed Al Sakiti <mohammed110051@gmail.com>
2023-07-15 19:14:45 +0400

    Task 3: Phone Book

---
+++ Contacts.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Dictionary
+{
+    internal class Contacts
+    {
+        private Dictionary<String, String> contactInfo;
+
+        public Contacts() 
+        {
+            contactInfo = new Dictionary<String, String>();
+        }
+        public void Add(String name, String phoneNum)
+        {
+            if (contactInfo.ContainsKey(name))
+            {
+                // If the name is already taken
+                Console.WriteLine("The name {0} already exists in the phone book.", name);
+            }
+            else
+            {
+                contactInfo.Add(name, phoneNum);
+                Console.WriteLine("The contact {0} with number {1} has successfully added to the phone book.", name, phoneNum);
+            }
+        }
+        public void Remove(String name)
+        {
+            if (contactInfo.ContainsKey(name))
+            {
+                contactInfo.Remove(name);
+
+                Console.WriteLine("The contact {0} has been deleted from the phone book.", name);
+            }
+            else
+            {
+                Console.WriteLine("The contact {0} is not found in the phone book.", name);
+            }
+        }
+        public void Search (String name)
+        {
+            if (contactInfo.ContainsKey(name))
+            {
+                string number = contactInfo[name];
+                Console.WriteLine("The contact name: {0} \r\nPhone number: {1}.", name, number);
+            }
+            else
+            {
+                Console.WriteLine("The contact {0} is not found in the phone book.", name);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Diagnostics.Metrics;
 using static System.Net.Mime.MediaTypeNames;
 using System.Xml.Linq;
+using static System.Formats.Asn1.AsnWriter;
+using System.Collections;
+using System.Runtime.Intrinsics.X86;
 
 namespace cSharp_Dictionary
 {
@@ -19,8 +22,8 @@ namespace cSharp_Dictionary
             //personMap.Add(person2.Id, person2);
             //personMap.Add(person3.Id, person3);
             personMap[person1.Id] = person1; // Add method 2
-            personMap [person2.Id] = person2;
-            personMap [person3.Id] = person3;
+            personMap[person2.Id] = person2;
+            personMap[person3.Id] = person3;
             person1.Gender = "Female"; // Change the gender
             personMap[person1.Id] = person2; // Change something
             personMap.TryAdd(person1.Id, person1); // 
@@ -40,7 +43,7 @@ namespace cSharp_Dictionary
             string sen = Console.ReadLine();
 
             string[] words = sen.Split(" "); // To remove the space from count.
-            
+
             Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
             foreach (string word in words)
             {
@@ -91,13 +94,71 @@ namespace cSharp_Dictionary
             DisplayHashSet(GetUniqueElements(array2));
             Console.WriteLine("The unique elements in array3 are:");
             DisplayHashSet(GetUniqueElements(array3));
-        }
-        public static void DisplayHashSet(HashSet<int> set)
-        {
-            foreach (int element in set)
+
+            static void DisplayHashSet(HashSet<int> set)
             {
-                Console.WriteLine(element);
+                foreach (int element in set)
+                {
+                    Console.WriteLine(element);
+                }
             }
+            // Task 3: Phone Book
+            // Create a phone book program that allows users to add, search, and delete contacts.
+            // Use a Dictionary to store the contacts, where the keys are the names and the values
+            // are the phone numbers.Implement functions to add a contact, search for a contact by name,
+            // and delete a contact by name.
+            Console.WriteLine("\r\nTask 3: Phone Book\r\n~~~~~~~~~~~~~~~\n");
+            Console.WriteLine("    Welcome To    ");
+            Console.WriteLine(" +-+-+-+-+-+-+-+-+-+\r\n |W|h|a|t|I|s|F|u|n|\r\n +-+-+-+-+-+-+-+-+-+");
+            Console.WriteLine("Guesiing Number Game\n");
+            // Display menu
+            Console.WriteLine("Please choose an option:");
+            Console.WriteLine("1. Add a contact");
+            Console.WriteLine("2. Search for a contact by name");
+            Console.WriteLine("3. Delete a contact by name");
+            Console.WriteLine("4. Exit");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            // Loop until the user chooses to exit '4'
+            while (choice != 4)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter a name:");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter a number:");
+                        string phoneNum = Console.ReadLine();
+
+                        Contacts contacts = new Contacts();
+                        contacts.Add(name, phoneNum);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter a name:");
+                        string searchName = Console.ReadLine();
+                        contacts.Search(searchName);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter a name:");
+                        string deleteName = Console.ReadLine();
+                        contacts.Remove(deleteName);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+
+                Console.WriteLine("Please choose an option:");
+                Console.WriteLine("1. Add a contact");
+                Console.WriteLine("2. Search for a contact by name");
+                Console.WriteLine("3. Delete a contact by name");
+                Console.WriteLine("4. Exit");
+
+                choice = int.Parse(Console.ReadLine());
+            }
+            //Exit message
+            Console.WriteLine("Thank you for using WhatIsFun Phone Book. \r\nGoodbye!");
         }
     }
 }
